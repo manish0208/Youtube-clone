a4378e9db450dc6b0505e56c5991475d9e347eff
Manish Veerani <manishveerani0208@gmail.com>
2022-09-04 17:30:02 +0530

    setup VideoItem

--- src/Components/VideoList.jsx
+++ src/Components/VideoList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos, onSelectVideo}) => {
+const VideoList = ({videos, onVideoSelect}) => {
     const listOfVideos = videos.map(video => (
         <VideoItem
-            onSelectVideo={onSelectVideo}
-            key={video.VideoItem.videoId}
+            onVideoSelect={onVideoSelect}
+            key={video.id.videoId}
             video={video}
         />
     ));
